2fdc64585610f94eab276d6f900087cb91fe65d5
Marco Fraccaroli <marco.fraccaroli@mwd.digital>
2018-10-24 17:48:31 +0200

    Updated Android proximitysdk library to 1.6.0

--- src/android/KaribooActivity.java
+++ src/android/KaribooActivity.java
@@ -12,38 +12,44 @@ import org.apache.cordova.*;
 
 
 class CheckPermission {
-    public static boolean checkPermission(Activity activity){
-        int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION);
-        if (result == PackageManager.PERMISSION_GRANTED){
-            return true;
-        } else {
-            return false;
-        }
+  public static boolean checkPermission(Activity activity){
+    int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION);
+    if (result == PackageManager.PERMISSION_GRANTED) {
+      return true;
+    } else {
+      return false;
     }
-    public static void requestPermission(Activity activity, final int code){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.ACCESS_FINE_LOCATION)){
-            Toast.makeText(activity, "Questa applicazione ha bisogno di accedere alla tua posizione per fornirti un'esperienza ottimale.", Toast.LENGTH_LONG).show();
-        } else {
-            ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},code);
-        }
+  }
+  public static void requestPermission(Activity activity, final int code){
+    if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.ACCESS_FINE_LOCATION)) {
+      Toast.makeText(activity, "Questa applicazione ha bisogno di accedere alla tua posizione per fornirti un'esperienza ottimale.", Toast.LENGTH_LONG).show();
+    } else {
+      ActivityCompat.requestPermissions(activity,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},code);
     }
+  }
 }
 
-public class KaribooActivity extends CordovaActivity
-{
-    public final static int TAG_PERMISSION_CODE = 1;
+public class KaribooActivity extends CordovaActivity {
+  public static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
-            moveTaskToBack(true);
-        }
-        loadUrl(launchUrl);
-        if(!CheckPermission.checkPermission(KaribooActivity.this)) {
-            CheckPermission.requestPermission(KaribooActivity.this,TAG_PERMISSION_CODE);
-        }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Bundle extras = getIntent().getExtras();
+    if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+      moveTaskToBack(true);
     }
+    loadUrl(launchUrl);
+    if(!CheckPermission.checkPermission(KaribooActivity.this)) {
+      CheckPermission.requestPermission(KaribooActivity.this,LOCATION_PERMISSION_REQUEST_CODE);
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+      ProximitySDK.getInstance().checkPendingPermissions();
+    }
+  }
 }
--- src/android/KaribooApplication.java
+++ src/android/KaribooApplication.java
@@ -7,23 +7,15 @@ import com.jointag.proximity.ProximitySDK;
 import com.jointag.proximity.listener.CustomActionListener;
 import com.jointag.proximity.util.Logger;
 
-public class KaribooApplication extends Application implements CustomActionListener {
+public class KaribooApplication extends Application {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        String apiKey = "@KARIBOO_ID@";
-        String apiSecret = "@KARIBOO_SECRET@";
-        Logger.setPrefix("KaribooApplication.");
-        Logger.setLogLevel(Logger.VERBOSE);
-        // Uncomment to enable verbose debug logs:
-        // ProximitySDK.setDebug(true);
-        ProximitySDK.init(this, apiKey, apiSecret);
-        ProximitySDK.getInstance().addCustomActionListener(this);
-    }
-
-    @Override
-    public void onCustomAction(String s) {
-        Log.d("Kariboo", "Received custom action " + s);
-    }
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    String apiKey = "@KARIBOO_ID@";
+    String apiSecret = "@KARIBOO_SECRET@";
+    Logger.setPrefix("KaribooApplication.");
+    Logger.setLogLevel(Logger.VERBOSE);
+    ProximitySDK.init(this, apiKey, apiSecret);
+  }
 }
